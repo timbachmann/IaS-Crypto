cfa02fea3f42a0e6bd8edc648bc7fc7ac4fe4692
raphael <r.kreft@unibas.ch>
2021-04-15 13:56:54 +0200

    finished task 3

--- src/iascrypto/aead-encrypt.py
+++ src/iascrypto/aead-encrypt.py
@@ -1,5 +1,17 @@
 #!/bin/python3
 
+"""
+AEAD is a standard that deinesn how to encrypt and sign files/data. It Uses AES with CBC Method and 
+HMAC Hash to sign the data. 
+
+This script takes an input file, a header and an output path.
+It encrypts and signs the input file and writes it to the output file/path.
+
+use head -c 54 ursprungsbild.bmp > myheader.bmp to save header to seperate file.
+Then Append encrypted/signed output to header: cat myoutputfile.bmp >> myheader.bmp
+-> This is the complete resulted encrypted file
+"""
+
 import argparse
 from aead import AEAD
 
@@ -26,17 +38,19 @@ def main(args):
     cipher_text = cipher.encrypt(plain_text, header)
     print(header)
 
-    #write_encrypted(cipher_text, args.output)
+    write_encrypted(cipher_text, args.output)
 
-    header.replace(b'\x00', b'\x01')
+    # Uncomment to change header and test the validation of signature
+    # header += b'AAA'
 
     print(header)
+    # Comment In to test validation
     cipher.decrypt(cipher_text, header)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='AEAD Encryption')
     parser.add_argument('-i', '--input', type=str, help="Path to plain file", required=True)
     parser.add_argument('-header', '--header', type=str, help="Path to header file", required=True)
-    parser.add_argument('-o', '--output', type=str, help="Path of encrypted file (should not exist)", required=True)
+    parser.add_argument('-o', '--output', type=str, help="Path of encrypted file (should not exist)")
     args = parser.parse_args()
     main(args)
